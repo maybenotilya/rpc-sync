a403c0ca2fdb2a932b1fc99ef4e513e0a6ff707c
Ilya Syresenkov <ilya.syresenkov.04@gmail.com>
2025-12-10 22:53:48 +0300

    non working impl

--- main.cpp
+++ main.cpp
@@ -1,130 +1,163 @@
+#include <arpa/inet.h>
 #include <chrono>
-#include <grpcpp/grpcpp.h>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-#include <memory>
+#include <rpc/rpc.h>
 #include <string>
 #include <thread>
+#include <unistd.h>
 
-#include "rpc_sync.grpc.pb.h"
+#include "state.h"
 
-using namespace grpc;
+enum State : uint8_t { Sleep = 0, Run = 1 };
 
-RPCSync::State State;
+State state;
+State startingState;
+char localhost[] = "localhost";
 
-class PingServiceImpl final : public RPCSync::PingService::Service {
-public:
-    Status Ping(ServerContext* context, RPCSync::PingRequest const* request,
-                RPCSync::PingResponse* response) override {
-        std::cout << "Received ping from other process" << std::endl;
-
-        if (State == RPCSync::State::Sleep) {
-            State = RPCSync::State::Run;
-            std::cout << "State changed: sleep -> run" << std::endl;
-        } else {
-            State = RPCSync::State::Sleep;
-            std::cout << "State changed: run -> sleep" << std::endl;
-        }
-
-        return Status::OK;
-    }
-};
-
-class PingClient {
-public:
-    PingClient(std::shared_ptr<Channel> channel) : stub_(RPCSync::PingService::NewStub(channel)) {}
-
-    bool SendPing() {
-        RPCSync::PingRequest request;
-
-        RPCSync::PingResponse response;
-        ClientContext context;
-
-        std::chrono::system_clock::time_point deadline =
-                std::chrono::system_clock::now() + std::chrono::seconds(5);
-        context.set_deadline(deadline);
-
-        Status status = stub_->Ping(&context, request, &response);
-
-        if (status.ok()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-private:
-    std::unique_ptr<RPCSync::PingService::Stub> stub_;
-};
-
-void RunServer(std::string const& address) {
-    PingServiceImpl service;
-
-    ServerBuilder builder;
-    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on " << address << std::endl;
-
-    server->Wait();
+bool IsRun() {
+    return state == State::Run;
 }
 
-void RunClient(std::string const& address) {
-    // Wait for server to run
+int GetSelfProg() {
+    return startingState == State::Run ? STATE_PROG + 1 : STATE_PROG;
+}
+
+int GetOtherProg() {
+    return startingState == State::Sleep ? STATE_PROG + 1 : STATE_PROG;
+}
+
+#undef STATE_PROG
+
+void* state_proc_1_svc(void*, struct svc_req*) {
+    if (state != State::Sleep) {
+        return nullptr;
+    }
+    std::cout << "Message recieved" << std::endl;
+    state = State::Run;
+    std::cout << "State changed: " << (state ? "Run" : "Sleep") << std::endl;
+
+    return nullptr;
+}
+
+bool SendMessage() {
+    CLIENT* clnt;
+
+    clnt = clnt_create(localhost, GetOtherProg(), STATE_VERS, "tcp");
+    if (clnt == nullptr) {
+        clnt_pcreateerror(localhost);
+        return false;
+    }
+
+    if (state_proc_1(nullptr, clnt) == nullptr) {
+        std::cerr << "Failed to send message" << std::endl;
+
+        return false;
+    }
+
+    clnt_destroy(clnt);
+    return true;
+}
+
+void state_prog_1(struct svc_req* rqstp, SVCXPRT* transp) {
+    union {
+        int fill;
+    } argument;
+
+    char* result;
+    xdrproc_t _xdr_argument, _xdr_result;
+    char* (*local)(char*, struct svc_req*);
+
+    switch (rqstp->rq_proc) {
+        case NULLPROC:
+            (void)svc_sendreply(transp, (xdrproc_t)xdr_void, (char*)NULL);
+            return;
+
+        case STATE_PROC:
+            _xdr_argument = (xdrproc_t)xdr_void;
+            _xdr_result = (xdrproc_t)xdr_void;
+            local = (char* (*)(char*, struct svc_req*))state_proc_1_svc;
+            break;
+
+        default:
+            svcerr_noproc(transp);
+            return;
+    }
+    memset((char*)&argument, 0, sizeof(argument));
+    if (!svc_getargs(transp, (xdrproc_t)_xdr_argument, (caddr_t)&argument)) {
+        svcerr_decode(transp);
+        return;
+    }
+    result = (*local)((char*)&argument, rqstp);
+    if (result != NULL && !svc_sendreply(transp, (xdrproc_t)_xdr_result, result)) {
+        svcerr_systemerr(transp);
+    }
+    if (!svc_freeargs(transp, (xdrproc_t)_xdr_argument, (caddr_t)&argument)) {
+        fprintf(stderr, "%s", "unable to free arguments");
+        exit(1);
+    }
+    return;
+}
+
+void rpc_server() {
+    SVCXPRT* transp;
+
+    pmap_unset(GetSelfProg(), STATE_VERS);
+
+    transp = svctcp_create(RPC_ANYSOCK, 0, 0);
+    if (transp == nullptr) {
+        std::cerr << "Failed to create transport handle" << std::endl;
+        return;
+    }
+
+    if (!svc_register(transp, GetSelfProg(), STATE_VERS, state_prog_1, IPPROTO_TCP)) {
+        std::cerr << "Failed to register server" << std::endl;
+        return;
+    }
+
+    svc_run();
+
+    return;
+}
+
+void StartSync() {
+    std::thread server_thread(rpc_server);
+    server_thread.detach();
+
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-
-        if (State == RPCSync::State::Run) {
-            std::cout << "Sending ping" << std::endl;
-
-            auto channel = grpc::CreateChannel(address, grpc::InsecureChannelCredentials());
-            PingClient client(channel);
-
-            bool success = client.SendPing();
-
-            if (success) {
-                State = RPCSync::State::Sleep;
-                std::cout << "State changed: run -> sleep" << std::endl;
-            } else {
-                std::cout << "Failed to send ping, retrying..." << std::endl;
+        if (IsRun()) {
+            std::cout << "Sending message" << std::endl;
+            if (!SendMessage()) {
+                std::cout << "Failed to send" << std::endl;
+                continue;
             }
+            state = State::Sleep;
+            std::cout << "State changed: " << (state ? "Run" : "Sleep") << std::endl;
         }
     }
 }
 
-int main(int argc, char** argv) {
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <clientPort> <targetPort> <initialState>"
-                  << std::endl;
-        std::cerr << "Example: " << argv[0] << " 1234 4321 sleep" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " [sleep|run]" << std::endl;
         return 1;
     }
 
-    std::string clientPort = argv[1];
-    std::string targetPort = argv[2];
-    std::string initialState = argv[3];
-
-    if (initialState == "run") {
-        State = RPCSync::State::Run;
-    } else if (initialState == "sleep") {
-        State = RPCSync::State::Sleep;
+    if (strcmp(argv[1], "sleep") == 0) {
+        state = State::Sleep;
+    } else if (strcmp(argv[1], "run") == 0) {
+        state = State::Run;
     } else {
-        std::cerr << "Initial state must be 'run' or 'sleep'" << std::endl;
+        std::cout << "Unrecognized state: " << argv[1] << std::endl;
         return 1;
     }
 
-    std::cout << "Starting with state: " << initialState << std::endl;
-    std::cout << "Client port: " << clientPort << ", Target port: " << targetPort << std::endl;
+    startingState = state;
 
-    std::string clientAddress = "localhost:" + clientPort;
-    std::string targetAddress = "localhost:" + targetPort;
-
-    std::thread server_thread([&clientAddress]() { RunServer(clientAddress); });
-    server_thread.detach();
-
-    RunClient(targetAddress);
+    StartSync();
 
     return 0;
 }
---
+++ state.x
@@ -0,0 +1,5 @@
+program STATE_PROG {
+    version STATE_VERS {
+        void STATE_PROC(void) = 1;
+    } = 1;
+} = 0x20000001;
